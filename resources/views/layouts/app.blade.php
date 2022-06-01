18aa9ae5050bf9b101b734670a05be9b698996a8
Hildhan123 <hildhan123@gmail.com>
2022-06-01 15:15:40 +0700

    Login clear

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,81 +3,192 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <!-- ========== Favicon Ico ========== -->
+    <link rel="icon" href="../assets/img/favicon.png">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Login</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet">
+    {{-- <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet"> --}}
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <style media="screen">
+        *,
+        *:before,
+        *:after {
+          padding: 0;
+          margin: 0;
+          box-sizing: border-box;
+        }
+    
+        body {
+            background-image: url('../assets/img/background.jpg'); 
+            background-size: 1920px 1080px;
+        }
+    
+        .background {
+          width: 430px;
+          height: 520px;
+          position: absolute;
+          transform: translate(-50%, -50%);
+          left: 50%;
+          top: 50%;
+        }
+    
+        .background .shape {
+          height: 200px;
+          width: 200px;
+          position: absolute;
+          border-radius: 50%;
+        }
+    
+        .shape:first-child {
+          background: linear-gradient(#1845ad,
+              #23a2f6);
+          left: -80px;
+          top: -80px;
+        }
+    
+        .shape:last-child {
+          background: linear-gradient(to right,
+              #ff512f,
+              #f09819);
+          right: -30px;
+          bottom: -80px;
+        }
+    
+        form {
+          height: auto;
+          width: 400px;
+          background-color: rgba(255, 255, 255, 0.13);
+          position: relative;
+          /* transform: translate(50%, 50%); */
+          top: 100px;
+          /* bottom: -250px; */
+          /* left: 50%; */
+          border-radius: 10px;
+          backdrop-filter: blur(10px);
+          border: 2px solid rgba(255, 255, 255, 0.1);
+          box-shadow: 0 0 40px rgba(8, 7, 16, 0.6);
+          padding: 50px 50px;
+          margin: auto;
+        }
+
+        .mantap{
+            height: auto;
+          width: 400px;
+          background-color: rgba(255, 255, 255, 0.13);
+          position: absolute;
+          transform: translate(-50%, -50%);
+          top: 50%;
+          left: 50%;
+          border-radius: 10px;
+          backdrop-filter: blur(10px);
+          border: 2px solid rgba(255, 255, 255, 0.1);
+          box-shadow: 0 0 40px rgba(8, 7, 16, 0.6);
+          padding: 50px 35px;
+        }
+    
+        form * {
+          font-family: 'Poppins', sans-serif;
+          color: #ffffff;
+          letter-spacing: 0.5px;
+          outline: none;
+          border: none;
+        }
+    
+        form h3 {
+          font-size: 32px;
+          font-weight: 500;
+          line-height: 42px;
+          text-align: center;
+        }
+    
+        label {
+          display: block;
+          margin-top: 30px;
+          font-size: 16px;
+          font-weight: 500;
+        }
+    
+        input {
+          display: block;
+          height: 50px;
+          width: 100%;
+          background-color: rgba(255, 255, 255, 0.07);
+          border-radius: 3px;
+          padding: 0 10px;
+          margin-top: 8px;
+          font-size: 14px;
+          font-weight: 300;
+        }
+        select{
+        display: block;
+          height: 50px;
+          width: 100%;
+          background-color: rgba(255, 255, 255, 0.07);
+          border-radius: 3px;
+          padding: 0 10px;
+          margin-top: 8px;
+          font-size: 14px;
+          font-weight: 300;
+        }
+    
+        ::placeholder {
+          color: #e5e5e5;
+        }
+    
+        button {
+          margin-top: 50px;
+          width: 100%;
+          background-color: #ffffff;
+          color: #080710;
+          padding: 15px 0;
+          font-size: 18px;
+          font-weight: 600;
+          border-radius: 5px;
+          cursor: pointer;
+        }
+    
+        .social {
+          margin-top: 30px;
+          display: flex;
+        }
+    
+        .social div {
+          background: red;
+          width: 150px;
+          border-radius: 3px;
+          padding: 5px 10px 10px 5px;
+          background-color: rgba(255, 255, 255, 0.27);
+          color: #eaf0fb;
+          text-align: center;
+        }
+    
+        .social div:hover {
+          background-color: rgba(255, 255, 255, 0.47);
+        }
+    
+        .social .fb {
+          margin-left: 25px;
+        }
+    
+        .social i {
+          margin-right: 4px;
+        }
+      </style>
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
+     @yield('content')
 </body>
 </html>
